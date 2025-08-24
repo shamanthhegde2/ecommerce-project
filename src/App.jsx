f98eaa7536aff3fcd749b780a28c1b8072445fbb
shamanth hegde <shamanthhegde12@gmail.com>
2025-08-23 19:20:06 -0500

    Used Link to route internally

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { Routes, Route } from "react-router-dom"
 
 import HomePage from "./pages/HomePage"
 import CheckoutPage from "./pages/CheckoutPage"
+import OrdersPage from "./pages/OrdersPage"
+import TrackingPage from "./pages/TrackingPage"
 
 import "./App.css"
-
-
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
     </Routes>
   )
 }
